53ffec160590d51ca1fe5758bb16cb084acca5ac
bjpatel18 <bjpatel18@myseneca.ca>
2025-05-18 16:59:57 -0400

    Completed Lab 2 with all scripts and laboutput.txt

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Taksh Brijesh Patel
+# Author ID: bjpatel18
+# Date Created: 2025/05/18
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author: Taksh Brijesh Patel
+# Author ID: bjpatel18
+# Date Created: 2025/05/18
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
+
